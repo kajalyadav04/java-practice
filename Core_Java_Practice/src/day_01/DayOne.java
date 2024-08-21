1899a1d393897e6993039cc877f001c6d94fbf04
kajalyadav04 <kajalraj1520@gmail.com>
2024-08-22 00:01:42 +0530

    done

--- Core_Java_Practice/src/day_01/DayOne.java
+++ Core_Java_Practice/src/day_01/DayOne.java
@@ -1,5 +1,14 @@
 package day_01;
 
 public class DayOne {
+	public static void main(String args[]) {
+		String name = "kajal";
+		String Address = "Jharkhand";
+		System.out.println("This is My First Java Application");
+		System.out.println("==================================");
+		System.out.println("My Name is:" + name);
+		System.out.println("My Address is:" + Address);
+
+	}
 
 }
---
+++ Core_Java_Practice/src/day_01/PrintProgram.java
@@ -0,0 +1,10 @@
+package day_01;
+
+public class PrintProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
