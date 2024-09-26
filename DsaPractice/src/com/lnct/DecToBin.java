c6727521df99b1fdf64a8343451a196795e0fb2f
kajalyadav04 <kajalraj1520@gmail.com>
2024-09-26 08:40:51 +0530

    DecToBin using ArrayList

--- DsaPractice/src/com/lnct/DecToBin.java
+++ DsaPractice/src/com/lnct/DecToBin.java
@@ -1,27 +1,44 @@
 package com.lnct;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
 
 public class DecToBin {
-	// Decimal to Binary
+
 	public static String decimalToBinary(int n) {
 		String binString = "";
 		StringBuilder stringBuilder = new StringBuilder("");
-//		int[] arr= new i
+
 		while (n > 0) {
 			int rem = n % 2;
 			stringBuilder.append(rem);
-//			i++;
 			n = n / 2;
 		}
-//		System.out.println(Arrays.toString(arr));
+
 		return stringBuilder.reverse().toString();
 
-//		return binString;
 	}
 
-	private static void deciToBin(int n) {
+	private static void decToBinUsingList(int n) {
+		// TODO Auto-generated method stub
+		List<Integer> arrList = new ArrayList<>();
+		int i = 0;
+		while (n > 0) {
+			int rem = n % 2;
+			arrList.add(rem);
+			i++;
+			n = n / 2;
+		}
+		for (int k = i - 1; k >= 0; k--) {
+			System.out.print(arrList.get(k));
+		}
+//		System.out.println(arrList);
+
+	}
+
+	private static void deciToBinUsingArray(int n) {
 		// TODO Auto-generated method stub
 		int[] arr = new int[1000];
 		int i = 0;
@@ -43,8 +60,10 @@ public class DecToBin {
 		int n = 30;
 		String res = decimalToBinary(n);
 		System.out.println(res);
-		deciToBin(n);
-		
+		deciToBinUsingArray(n);
+		System.out.println();
+		decToBinUsingList(n);
+
 	}
 
 }
