1db012c25df3d3990e29fe2f7017f9d7b1dc6d56
kajalyadav04 <kajalraj1520@gmail.com>
2024-09-26 16:21:58 +0530

    checking empty string

--- DsaPractice/src/com/lnct/Check_String_Empty.java
+++ DsaPractice/src/com/lnct/Check_String_Empty.java
@@ -4,21 +4,38 @@ public class Check_String_Empty {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String string ="k";
-		boolean k= check_String_Empty(string);
-		System.out.println(k);
+		String string ="ijk";
+//		boolean k= check_String_Empty(string);
+//		System.out.println(k);
+		boolean l=isEmptyUsingCharAt(string);
+		
+		System.out.println(l);
 
 	}
+	
+
+	private static boolean isEmptyUsingCharAt(String str) {
+		// TODO Auto-generated method stub
+		try {
+			char a=str.charAt(0);
+		} catch (Exception e) {
+			// TODO: handle exception
+			return true;
+		}
+		return false;
+	}
+
 
 	private static boolean check_String_Empty(String str) {
 		// TODO Auto-generated method stub
 		str=str.replaceAll("//s","");
 		char[] charArr=str.toCharArray();
-		int count=0;
+		
 		if(charArr.length==0) {
 			return true;
 		}
 		return false;
 	}
+	
 
 }
