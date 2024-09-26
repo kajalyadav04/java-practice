4bc2953c6cef5e768b1de0f585007ad4b5dd3e5c
kajalyadav04 <kajalraj1520@gmail.com>
2024-09-26 08:20:37 +0530

    BinToDec using array

--- DsaPractice/src/com/lnct/DecToBin.java
+++ DsaPractice/src/com/lnct/DecToBin.java
@@ -1,31 +1,50 @@
 package com.lnct;
 
 import java.util.Arrays;
+import java.util.Iterator;
 
 public class DecToBin {
 	// Decimal to Binary
 	public static String decimalToBinary(int n) {
-		String binString="";
+		String binString = "";
 		StringBuilder stringBuilder = new StringBuilder("");
 //		int[] arr= new i
-		while(n>0) {
-			int rem=n%2;
+		while (n > 0) {
+			int rem = n % 2;
 			stringBuilder.append(rem);
 //			i++;
-			n=n/2;	
+			n = n / 2;
 		}
 //		System.out.println(Arrays.toString(arr));
-		 return stringBuilder.reverse().toString();
-		
+		return stringBuilder.reverse().toString();
+
 //		return binString;
+	}
+
+	private static void deciToBin(int n) {
+		// TODO Auto-generated method stub
+		int[] arr = new int[1000];
+		int i = 0;
+		while (n > 0) {
+			int rem = n % 2;
+			arr[i] = rem;
+			i++;
+			n = n / 2;
+		}
+		for (int k = i - 1; k >= 0; k--) {
+			System.out.print(arr[k]);
+		}
+
 	}
 
 	public static void main(String[] args) {
 
 		// TODO Auto-generated method stub
 		int n = 30;
-		String res=decimalToBinary(n);
+		String res = decimalToBinary(n);
 		System.out.println(res);
+		deciToBin(n);
+		
 	}
 
 }
