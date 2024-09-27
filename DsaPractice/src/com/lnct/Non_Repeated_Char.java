17f7475c99f12cf7227e10cd6775f77f601e3bad
kajalyadav04 <kajalraj1520@gmail.com>
2024-09-27 15:11:13 +0530

    non repeated string using hashmap

--- DsaPractice/src/com/lnct/Non_Repeated_Char.java
+++ DsaPractice/src/com/lnct/Non_Repeated_Char.java
@@ -1,13 +1,39 @@
 package com.lnct;
 
-
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.HashMap;
 
 public class Non_Repeated_Char {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String string = "AAACDEEF";
+		String string = "AAACcDEEF";
+		string=string.toUpperCase();
 		repeat(string);
+		non_Repeated_Char(string);
+
+	}
+
+	private static void non_Repeated_Char(String str) {
+		// TODO Auto-generated method stub
+
+		Map<Character, Integer> map = new HashMap();
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			if (map.containsKey(ch)) {
+				map.put(ch, map.get(ch) + 1);
+			} else {
+				map.put(ch, 1);
+			}
+
+		}
+		for (Entry<Character, Integer> e : map.entrySet()) {
+			if (e.getValue() == 1) {
+				System.out.println(e.getKey());
+				break;
+			}
+		}
 
 	}
 
@@ -25,7 +51,7 @@ public class Non_Repeated_Char {
 			}
 			if (unique) {
 				System.out.println(string.charAt(i));
-                 break;
+				break;
 			}
 
 		}
