73dd3586959f64cc736b2368e85bd19d579219a3
kajalyadav04 <kajalraj1520@gmail.com>
2024-09-28 00:56:44 +0530

    try with resource in jdbc

--- Spring_Jdbc/src/com/jdbc/JavaConnectivity.java
+++ Spring_Jdbc/src/com/jdbc/JavaConnectivity.java
@@ -6,10 +6,17 @@ public class JavaConnectivity {
 
 	public static void main(String[] args) {
 		String urlString = "jdbc:mysql://localhost:3306/jdbcdb";
-		Connection connection = null;
+//		Connection connection = null;
+		
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			connection = DriverManager.getConnection(urlString,"root","root");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try(Connection connection = DriverManager.getConnection(urlString,"root","root")) {
+			
+//			connection = DriverManager.getConnection(urlString,"root","root");
 			System.out.println(connection);
 			connection.close();
 
