6518cc03d7e5806cd341e4e28a6398f898692308
kajalyadav04 <kajalraj1520@gmail.com>
2024-09-27 23:04:57 +0530

    jdbc program to update insert column

--- Spring_Jdbc/src/com/jdbc/CreateStatement.java
+++ Spring_Jdbc/src/com/jdbc/CreateStatement.java
@@ -7,36 +7,70 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
 
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.IconifyAction;
+
 public class CreateStatement {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
-		int age =scanner.nextInt();
-		String name=scanner.next();
-		String addreString=scanner.next();
-		int mobile=scanner.nextInt();
-		String urlString = "jdbc:mysql://localhost:3306/jdbcdb";
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection connection = DriverManager.getConnection(urlString, "root", "root");
-			Statement s = connection.createStatement();
-			String staString ="insert into student values("+age+",'"+name+"','"+addreString+"',"+mobile+")";
-			System.out.println(staString);
-			int rows= s.executeUpdate(staString);
-			if(rows>0) {
-				System.out.println("rows affected succesfully");
-			}
-			else {
-				System.out.println("rows remain unaffected");
-			}
-			connection.close();
+		int n;
+		do {
+			System.out.println("enter age");
+			int age = scanner.nextInt();
+			System.out.println("name");
+			String name = scanner.next();
+			System.out.println("address");
+			String addreString = scanner.next();
+			System.out.println("mobile");
+			int mobile = scanner.nextInt();
+			System.out.println("marks");
+			int marks = scanner.nextInt();
+			String urlString = "jdbc:mysql://localhost:3306/jdbcdb";
+			try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				Connection connection = DriverManager.getConnection(urlString, "root", "root");
+				Statement s = connection.createStatement();
+				String staString = "insert into student values(" + age + ",'" + name + "','" + addreString + "',"
+						+ mobile + "," + marks + ")";
 
-		} catch (ClassNotFoundException | SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+				System.out.println(staString);
+				int rows = s.executeUpdate(staString);
+				if (rows > 0) {
+					System.out.println("rows affected succesfully");
+				} else {
+					System.out.println("rows remain unaffected");
+				}
 
+//				toAddColumn(s);
+				graceMarks(s);
+
+				connection.close();
+
+			} catch (ClassNotFoundException | SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			System.out.println("if you want to exit the program press 0 otherwise any num");
+			System.out.println("enter value ");
+			n = scanner.nextInt();
+
+		} while (n != 0);
 	}
 
+	public static void graceMarks(Statement statement) throws SQLException {
+		String queryString = "update student set marks=marks+500 where marks<=500";
+		int rows = statement.executeUpdate(queryString);
+
+		System.out.println(rows);
+	}
+
+	public static void toAddColumn(Statement statement) throws SQLException {
+		String queryString = "alter table student add marks int";
+		int rows = statement.executeUpdate(queryString);
+
+		System.out.println(rows);
+
+	}
 }
