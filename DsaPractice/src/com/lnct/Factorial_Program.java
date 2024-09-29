d0d34d7fcf08566027cacfefe628e37eeac5f4c5
kajalyadav04 <kajalraj1520@gmail.com>
2024-09-29 18:15:57 +0530

    factorial program without recursion

---
+++ DsaPractice/src/com/lnct/Factorial_Program.java
@@ -0,0 +1,20 @@
+package com.lnct;
+
+public class Factorial_Program {
+   
+   public static void factorial_Program(int n){
+	   int fact=1;
+    	for(int i=1;i<=5;i++) {
+    		fact=fact*i;
+    	}
+    	System.out.println(fact);
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int n=5;
+		  
+		factorial_Program(5);
+
+	}
+
+}
