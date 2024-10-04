681ea536bf397e0af5ffb35015e40dc9a4d99935
kajalyadav04 <kajalraj1520@gmail.com>
2024-10-04 23:05:21 +0530

    date time formatter

---
+++ Core_Java_Practice/src/com/day_13/DateTime.java
@@ -0,0 +1,24 @@
+package com.day_13;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class DateTime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter Date of birth in dd-MM-yyyy pattern");
+		String dobString=scanner.next();
+		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+		LocalDate lDate=LocalDate.parse(dobString, dateTimeFormatter);
+		System.out.println(lDate);
+		DateTimeFormatter dayDateTimeFormatter= DateTimeFormatter.ofPattern("EEEE");
+		String ldateString = lDate.format(dayDateTimeFormatter);
+			System.out.println(ldateString);
+		
+
+	}
+
+}
