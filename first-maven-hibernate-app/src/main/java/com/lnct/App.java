258febd99047d374b129d7e3c68c5ff68cd0ab5f
kajalyadav04 <kajalraj1520@gmail.com>
2024-10-09 01:35:30 +0530

    crud operation

--- first-maven-hibernate-app/src/main/java/com/lnct/App.java
+++ first-maven-hibernate-app/src/main/java/com/lnct/App.java
@@ -1,25 +1,66 @@
 package com.lnct;
 
+import javax.swing.border.EtchedBorder;
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
 /**
  * Hello world!
  *
  */
+
 public class App {
+	static EntityManagerFactory emf;
+	static {
+		emf = Persistence.createEntityManagerFactory("k");
+	}
+
 	public static void main(String[] args) {
-//        System.out.println( "Hello World!" );
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("book_connection");
-		EntityManager eManager = emf.createEntityManager();
-		Student student= eManager.find(Student.class, 2);
-		if(student!=null) {
-			System.out.println(student);
+		// System.out.println( "Hello World!" );
+//		EntityManagerFactory emf = Persistence.createEntityManagerFactory("book_connection");
+//		EntityManager eManager = emf.createEntityManager();
+		searchStudentRecord(1);
+		saveStudentRecord();
+
+	}
+
+	private static void saveStudentRecord() {
+		// TODO Auto-generated method stub
+		EntityTransaction et=null;
+		EntityManager em=null;
+		try {
+
+			em = emf.createEntityManager();
+			Student student = new Student(6, "kay", "jh", "45", 890);
+			et = em.getTransaction();
+			et.begin();
+			em.persist(student);
+			et.commit();
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			et.rollback();
+			e.printStackTrace();
+		} finally {
+			em.close();
 		}
-		else {
+
+	}
+
+	public static void searchStudentRecord(int roll) {
+		EntityManager eManager = emf.createEntityManager();
+
+		Student student = eManager.find(Student.class, roll);
+
+		if (student != null) {
+			System.out.println(student);
+		} else {
 			System.out.println("student not found");
 		}
 		eManager.close();
+
 	}
 }
