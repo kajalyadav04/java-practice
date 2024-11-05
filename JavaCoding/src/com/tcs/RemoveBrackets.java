7e444d0a799a33a5b3b2d780d977175426082d5d
kajalyadav04 <kajalraj1520@gmail.com>
2024-11-05 19:26:49 +0530

    sum of num in a string

--- JavaCoding/src/com/tcs/RemoveBrackets.java
+++ JavaCoding/src/com/tcs/RemoveBrackets.java
@@ -1,5 +1,16 @@
 package com.tcs;
 
+import java.util.Scanner;
+
 public class RemoveBrackets {
+	public static void main (String[] args) {
+		 Scanner sc= new Scanner(System.in);
+		 String s=sc.nextLine();
+		 String k=removeBrackets(s);
+		 System.out.println(k);
+		}
+		public static String removeBrackets(String s){
+		    return s.replaceAll("[(){}\\[\\]]","");
+		}
 
 }
