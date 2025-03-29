42ea0316cc4ed84be0009ef0a051213c616335c8
kajalyadav04 <kajalraj1520@gmail.com>
2025-03-29 17:57:49 +0530

    palindrome tring using inbuild fn

--- Core_Java_Practice/src/com/tcs/palindromeString.java
+++ Core_Java_Practice/src/com/tcs/palindromeString.java
@@ -1,5 +1,38 @@
 package com.tcs;
 
 public class palindromeString {
+	public boolean isPalindrome(String s) {
+	       
+        String k = s.toLowerCase().replaceAll("[^a-z0-9]", "");
 
+ 
+         if(k.isEmpty()){
+            return true;
+        }
+        // k=s.replaceAll(":","");
+        StringBuilder a= new StringBuilder();
+        char[] b=k.toCharArray();
+        
+        for(int i=b.length-1;i>=0;i--){
+             a.append(k.charAt(i));
+        }
+        return k.equals(a.toString());
+          
+    }
+	public boolean isPalindromeUsingInbuildfn(String s) {
+	       
+        String k = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+
+ 
+         if(k.isEmpty()){
+            return true;
+        }
+        // k=s.replaceAll(":","");
+        StringBuilder a= new StringBuilder(k).reverse();
+        
+        return k.equals(a.toString());
+          
+    }
 }
+	
+
